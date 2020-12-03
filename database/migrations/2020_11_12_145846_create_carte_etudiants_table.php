5498ca4af1262b5c4acdcdcc4c9db9951fa31e7c
vfleischmann <victor.fleischmann99@gmail.com>
2020-12-03 14:01:53 +0100

    carteENC1

---
+++ database/migrations/2020_11_12_145846_create_carte_etudiants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarteEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carte_etudiants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nomEtudiant');
+            $table->integer('dateEntreeENC');
+            $table->integer('numeroTelephone');
+            $table->string('email')->unique();
+            $table->string('Section');
+            $table->string('Fichier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carte_etudiants');
+    }
+}
